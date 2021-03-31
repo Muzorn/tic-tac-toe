f7a64ced7574673f10f71be943f3fc4125fe67ad
Muzorn <jesusmgc.it@gmail.com>
2021-03-31 17:05:00 +0200

    Ahora los dos tipos de ficha pueden ponerse en el tablero a través de turnos, mostrando cuál es el actual.

--- src/index.js
+++ src/index.js
@@ -16,13 +16,22 @@ class Board extends React.Component {
 
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+
+    // Opción inicial mía
+    // (this.state.xIsNext) ? squares[i] = 'X' : squares[i] = 'O';
+    // Opción del cursillo
+    squares[i] = (this.state.xIsNext) ? 'X' : 'O';
+
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -35,7 +44,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
